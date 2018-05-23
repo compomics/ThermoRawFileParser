e7546b2e3ad8eab8da0d0977445db548e3afa1d6
nielshulstaert <nielshulstaert@gmail.com>
2018-05-23 14:27:47 +0200

    intermediate commit before release

---
+++ ThermoRawFileParserTest/FakeMsDataFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IO.MzML;
+using MassSpectrometry;
+
+namespace ThermoRawFileParserTest
+{
+    public class FakeMsDataFile : MsDataFile<IMzmlScan>, IMsStaticDataFile<IMzmlScan>
+    {
+        #region Public Constructors
+
+        public FakeMsDataFile(IMzmlScan[] FakeScans) : base(FakeScans,
+            new SourceFile(@"scan number only nativeID format", "mzML format", null, "SHA-1", @"C:\fake.mzML", null))
+        {
+            this.Scans = FakeScans;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public override int GetClosestOneBasedSpectrumNumber(double retentionTime)
+        {
+            int ok = Array.BinarySearch(Scans.Select(b => b.RetentionTime).ToArray(), retentionTime);
+            if (ok < 0)
+                ok = ~ok;
+            return ok + 1;
+        }
+
+        public override IEnumerable<IMzmlScan> GetMS1Scans()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IMzmlScan GetOneBasedScan(int scanNumber)
+        {
+            return Scans[scanNumber - 1];
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ ThermoRawFileParserTest/Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using IO.MzML;
+using MassSpectrometry;
+using MzLibUtil;
+using NUnit.Framework;
+
+namespace ThermoRawFileParserTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            //ThermoStaticData staticThermo = ThermoStaticData.LoadAllStaticData(@"spectra.raw");
+            //ThermoDynamicData dynamicThermo = ThermoDynamicData.InitiateDynamicConnection(@"spectra.raw")
+            //Mzml mzmlFile = Mzml.LoadAllStaticData(@"spectra.mzML");           
+            
+            //create temp file
+            String tempFileName = Path.GetTempPath() + "elements.dat";
+            //string tempFileName = Path.GetTempFileName();
+            Console.WriteLine("fdffffffffff" + File.Exists(tempFileName));
+            
+            UsefulProteomicsDatabases.Loaders.LoadElements(tempFileName);
+            
+            double[] intensities1 = new double[] { 120.3, 230.5, 780.6, 490.5};
+            double[] mz1 = new double[] { 16.3, 45.2, 78.5, 112.6 };
+            MzmlMzSpectrum massSpec1 = new MzmlMzSpectrum(mz1, intensities1, false);
+            IMzmlScan[] scans = new IMzmlScan[]{
+                new MzmlScan(1, massSpec1, 1, true, Polarity.Positive, 1, new MzRange(1, 100), "f", MZAnalyzerType.Orbitrap, massSpec1.SumOfAllY, null, "1")
+            };
+            
+            FakeMsDataFile f = new FakeMsDataFile(scans);
+            MzmlMethods.CreateAndWriteMyMzmlWithCalibratedSpectra(f, Path.Combine("/home/niels/Desktop/raw/test", "mzmlWithEmptyScan.mzML"), false);
+
+            Mzml ok = Mzml.LoadAllStaticData(Path.Combine("/home/niels/Desktop/raw/test", "mzmlWithEmptyScan.mzML"));
+            
+            MzmlMethods.CreateAndWriteMyMzmlWithCalibratedSpectra(ok, Path.Combine("/home/niels/Desktop/raw/test", "mzmlWithEmptyScan2.mzML"), false);
+
+            var testFilteringParams = new FilteringParams(200, 0.01, 5, true, true);
+            ok = Mzml.LoadAllStaticData(Path.Combine("/home/niels/Desktop/raw/test", "mzmlWithEmptyScan2.mzML"), testFilteringParams);
+            Assert.True(true);
+        }
+    }
+
+}
