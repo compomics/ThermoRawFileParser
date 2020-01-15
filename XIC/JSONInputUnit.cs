0f69fc1a648f83385edd0c0cf7d6058e8045b571
caetera <homer2k@gmail.com>
2020-01-15 14:57:16 +0100

    JSONParser

---
+++ XIC/JSONInputUnit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThermoRawFileParser.XIC
+{
+    public class JSONInputUnit
+    {
+        public double MzStart { get; set; }
+        public double MzEnd { get; set; }
+        public double Mz { get; set; }
+        public string Sequence { get; set; }
+        public double Tolerance { get; set; }
+        public string ToleranceUnit { get; set; }
+        public int Charge { get; set; }
+        public double RtStart { get; set; }
+        public double RtEnd { get; set; }
+    }
+}
---
+++ XIC/JSONParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ThermoRawFileParser.XIC
+{
+    public class JSONParser
+    {
+        private JSONInput jsonIn;
+
+        public JSONParser(string jsonPath)
+        {
+            XicData data = new XicData();
+            using (StreamReader sr = new StreamReader(jsonPath))
+            {
+                jsonIn = JsonConvert.DeserializeObject<JSONInput>(sr.ReadToEnd());
+            }
+        }
+
+
+    }
+}
