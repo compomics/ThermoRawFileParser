deb50cf33b9bf8ae04b24950b5edccf36369b847
caetera <homer2k@gmail.com>
2020-01-15 17:28:34 +0100

    Add tolerance

--- XIC/JSONParser.cs
+++ XIC/JSONParser.cs
@@ -21,6 +21,23 @@ namespace ThermoRawFileParser.XIC
 
             foreach (JSONInputUnit xic in jsonIn)
             {
+                if (xic.RtStart == 0) xic.RtStart = -1;
+                if (xic.RtEnd == 0) xic.RtEnd = -1;
+                if (xic.Tolerance != 0 && xic.ToleranceUnit != null && xic.Mz != 0)
+                {
+                    double delta;
+                    switch (xic.ToleranceUnit.ToLower())
+                    {
+                        case "ppm": delta = xic.Mz * xic.Tolerance * 1e-6; break;
+                        case "amu": delta = xic.Tolerance; break;
+                        case "mmu": delta = xic.Tolerance * 1e-3; break;
+                        case "da": delta = xic.Tolerance; break;
+                        default:
+                            throw new Exception(String.Format("Cannot parse tolerance unit: {0}", xic.ToleranceUnit));
+                    }
+                    data.content.Add(new XicUnit(xic.Mz - delta, xic.Mz + delta, xic.RtStart, xic.RtEnd));
+                }
+
                 if (xic.MzStart != 0 && xic.MzEnd != 0) data.content.Add(new XicUnit(xic.MzStart, xic.MzEnd, xic.RtStart, xic.RtEnd));
             }
 
